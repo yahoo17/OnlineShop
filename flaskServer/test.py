3a1bbd27d9897ea433e05b9f0a6bf9b61788635a
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-06-18 11:09:17 +0800

    1

--- flaskServer/test.py
+++ flaskServer/test.py
@@ -1,7 +1,16 @@
 from model2 import db,User
 
-stu=User.query.get(10)
-id=stu.id
-name=stu.name
-email=stu.email
-print(id,name,email)
+# stu=User.query.get(10)
+# id=stu.id
+# name=stu.name
+# email=stu.email
+# print(id,name,email)
+def func(a,b,c):
+
+    print("a= %d", a)
+    print("b= %d", b)
+    print("c= %d", c)
+
+
+
+func(b = 10, a = 3, c = 1)
